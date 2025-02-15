aa81f04db8e56146c6efc1488fb5b6085e62c3ea
joshHam <163796630+joshHam@users.noreply.github.com>
2025-02-15 13:50:24 +0000

    Initial commit with server configuration

--- app/caching.py
+++
@@ -1,5 +0,0 @@
-# caching.py
-
-from flask_caching import Cache
-
-cache = Cache()
--- cli/__init__.py
+++
@@ -1,9 +0,0 @@
-from .recount import recount
-from .admin import admin
-from .default import default
-from .migration import migration
-from .route import route
-from .translations import translations
-from .user import user
-
-commands = [migration, recount, route, admin, default, translations, user]
